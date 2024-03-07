14e53bf9d1fb5c8c570e27e0828ac626900fe64a
wilmarcabezas <wilmar.cabezas.pisco@gmail.com>
2024-03-06 20:47:50 -0500

    get con parametro nombre

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,4 +10,9 @@ router.get('/', async(req,res)=>{
     res.json(resultadoConsulta.rows);
 })
 
+router.get('/:nombre',async(req,res)=>{
+    const resultadoConsulta = await client.query(`select * from products where upper(nombre) like upper('%${req.params.nombre}%')`);
+    res.json(resultadoConsulta.rows);
+})
+
 export default router;
